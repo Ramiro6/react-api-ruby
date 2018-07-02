1f7a29a238122f6abcdc560258efa8ad4c7d919f
= <ramiroruiz578@gmail.com>
2018-07-02 22:59:04 +0200

    add react another

--- react-api/src/App.jsx
+++ react-api/src/App.jsx
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
 import Posts from './components/posts/containers/posts.jsx';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
+const store = createStore(
+  (state) => state,
+  {"id": 1, "title": "asljdhlajdsjdskljdasl"}
+)
 
 class App extends Component {
-
   render() {
     return (
-      <div>
-        <Posts />
+      <div className="App">
+        <Provider store={store}>
+          <Posts />
+        </Provider>
       </div>
     );
   }
--- react-api/src/components/posts/containers/posts.jsx
+++ react-api/src/components/posts/containers/posts.jsx
@@ -1,25 +1,23 @@
 import React, { Component } from 'react';
-import Post from '../components/post.jsx';
-
+import { connect } from 'react-redux';
 
 class Posts extends Component {
-  state = { data: [] }
-  componentWillMount() {
-    fetch('http://localhost:3000/api/posts').then((item) => {
-      return item.json()
-    }).then((res) => {
-      this.setState({data: res})
-    })
-  }
-
   render() {
     return(
-      <div className="container">
-        <h1>Posts</h1>
-        <Post data={this.state.data} />
+      <div>
+        {
+          console.log(this.props)
+        }
+        <h1>Posts!</h1>
       </div>
     )
   }
 }
 
-export default Posts
+const mapStateToProps = (state) => {
+  return {
+    data: state
+  }
+}
+
+export default connect(mapStateToProps)(Posts)
